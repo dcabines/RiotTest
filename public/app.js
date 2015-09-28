1981ee71e1c712ddfb4cf67dfeb0e05dccae527f
David Cabiness <dcabines@gmail.com>
2015-09-28 13:53:16 -0400

    cleanup

--- public/app.js
+++
@@ -1,8 +0,0 @@
-riot.mount('todo', {
-  title: 'Todo',
-  items: [
-    { title: 'Avoid excessive caffeine', done: true },
-    { title: 'Be less provocative', done: false  },
-    { title: 'Be nice to people', done: false }
-  ]
-});
