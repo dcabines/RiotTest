1f62cd759b1ed3ff7c97f37429c954d47a692696
David Cabiness <dcabines@gmail.com>
2015-09-28 13:20:06 -0400

    .

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 riot.mount('todo', {
   title: 'Todo',
   items: [
-    { title: 'Avoid excessive coffeine', done: true, hidden: false },
-    { title: 'Be less provocative', done: false, hidden: false  },
-    { title: 'Be nice to people', done: false, hidden: false }
+    { title: 'Avoid excessive caffeine', done: true },
+    { title: 'Be less provocative', done: false  },
+    { title: 'Be nice to people', done: false }
   ]
 });
--- server.js
+++ server.js
@@ -4,8 +4,6 @@ var app = express();
 app.use(express.static('.'));
 
 var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  var address = server.address();
+  console.log('Listening on port %d', address.port);
 });
