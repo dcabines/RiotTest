d3de1ce1c631a32e1148e6d07f061b1e9ecfcd44
David Cabiness <dcabines@gmail.com>
2015-10-25 15:19:55 -0400

    pretty tight

--- public/app.js
+++ public/app.js
@@ -10,14 +10,86 @@
     woodcutter: {
       unemployed: 1
     },
+    miner: {
+      unemployed: 1
+    },
     house: {
       wood: 5,
-      stone: 2
+      stone: 3
+    }
+  };
+  
+  var jobs = {
+    farmer: {
+      resource: 'food',
+      rate: 0.05
+    },
+    woodcutter: {
+      resource: 'wood',
+      rate: 0.025
+    },
+    miner: {
+      resource: 'stone',
+      rate: 0.02
+    }
+  };
+  
+  var actions = {
+    increment: {
+      create: function (resource, quantity) {
+        return {
+          name: 'increment',
+          resource: resource,
+          quantity: quantity
+        };
+      },
+      handler: function (state, action) {
+        var quantity = action.quantity || 1;
+        state[action.resource] += quantity;
+      }
+    },
+    work: {
+      create: function (worker) {
+        var job = jobs[worker];
+  
+        return {
+          name: 'work',
+          worker: worker,
+          rate: job.rate,
+          resource: job.resource
+        };
+      },
+      handler: function (state, action) {
+        var workers = state[action.worker];
+        state[action.resource] += workers * action.rate;
+      }
+    },
+    spawn: {
+      create: function (resource, quantity) {
+        return {
+          name: 'spawn',
+          resource: resource,
+          quantity: quantity || 1,
+          cost: prices[resource]
+        };
+      },
+      handler: function (state, action) {
+        var quantity = action.quantity;
+  
+        for (var resource in action.cost) {
+          var cost = action.cost[resource] * quantity;
+          state[resource] -= cost;
+        }
+  
+        state[action.resource] += quantity;
+      }
     }
   };
 
   w.App = {
     prices: prices,
+    jobs: jobs,
+    actions: actions,
     state: function (reducer, initialState, obj) {
       var state = initialState || {};
       obj = riot.observable(obj);
@@ -31,10 +103,42 @@
     },
     reducer: function (actions) {
       return function (state, action) {
-        //state = Object.create(state);
-        actions[action.name](state, action);
+        actions[action.name].handler(state, action);
         return state;
       };
+    }
+  };
+} (window, window.riot));
+
+(function (app, riot) {
+  var initialState = {
+    // resources
+    food: 0,
+    wood: 0,
+    stone: 0,
+    
+    // population
+    unemployed: 0,
+    farmer: 0,
+    woodcutter: 0,
+    miner: 0,
+    
+    // buildings
+    house: 0
+  };
+
+  var reducer = app.reducer(app.actions);
+  var store = app.state(reducer, initialState);
+
+  riot.mount('clicker', {
+    store: store,
+    action: function (resource) {
+      var prices = Object.keys(app.prices);
+      var hasPrice = prices.indexOf(resource) !== -1;
+      var name = hasPrice ? 'spawn' : 'increment';
+      var action = app.actions[name].create(resource);
+      
+      return action;
     },
     cost: function (action, state) {
       state = state || {};
@@ -49,106 +153,14 @@
       }
 
       return true;
-    },
-    actions: (function () {
-      var harvest = {
-        farmer: {
-          resource: 'food',
-          rate: 0.025
-        },
-        woodcutter: {
-          resource: 'wood',
-          rate: 0.05
-        }
-      };
-
-      var actions = {
-        'increment': function (resource, quantity) {
-          return {
-            name: 'increment',
-            resource: resource,
-            quantity: quantity
-          };
-        },
-        'work': function (worker) {
-          var config = harvest[worker];
-
-          return {
-            name: 'work',
-            worker: worker,
-            rate: config.rate,
-            resource: config.resource
-          };
-        },
-        'spawn': function (resource, quantity) {
-          return {
-            name: 'spawn',
-            resource: resource,
-            quantity: quantity,
-            cost: prices[resource]
-          };
-        }
-      };
-
-      return actions;
-    } ())
-  };
-} (window, window.riot));
-
-(function (app, riot) {
-  var reducer = app.reducer({
-    'increment': function (state, action) {
-      var quantity = action.quantity || 1;
-      state[action.resource] += quantity;
-    },
-    'work': function (state, action) {
-      var workers = state[action.worker];
-      state[action.resource] += workers * action.rate;
-    },
-    'spawn': function (state, action) {
-      var quantity = action.quantity || 1;
-
-      for (var resource in action.cost) {
-        var cost = action.cost[resource] * quantity;
-        state[resource] -= cost;
-      }
-
-      state[action.resource] += quantity;
-    }
-  });
-
-  var initialState = {
-    // resources
-    food: 0,
-    wood: 0,
-    stone: 0,
-    
-    // population
-    unemployed: 0,
-    farmer: 0,
-    woodcutter: 0,
-    
-    // buildings
-    house: 0
-  };
-
-  var workers = ['farmer', 'woodcutter'];
-  var store = app.state(reducer, initialState);
-
-  riot.mount('clicker', {
-    store: store,
-    action: function (resource) {
-      for (var key in app.prices) {
-        if(key === resource) return app.actions.spawn(resource);
-      }
-      
-      return app.actions.increment(resource);
     }
   });
 
   setInterval(function () {
-    workers.forEach(function (worker) {
-      var action = app.actions.work(worker);
+    var jobs = Object.keys(app.jobs);
+    
+    jobs.forEach(function (worker) {
+      var action = app.actions.work.create(worker);
       store.dispatch(action);
     });
   }, 250);
